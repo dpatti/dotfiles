c8a75646473244e3b80fcdc2e71c8e5b021cf715
Doug Patti <douglas.patti@gmail.com>
2020-12-19 17:16:57 -0500

    update from new computer
    We're giving up on nix for now

--- .config/nix/packages.nix
+++ .config/nix/packages.nix
@@ -1,18 +1,36 @@
-with import <nixpkgs> {}; {
-  # xorg = with xorg; {
-  #   inherit
-  #     xorgserver
-  #     xf86inputsynaptics
-  #     xf86videointel
-  #   ;
-  # };
-  # services.xserver.enable = true;
+with import <nixpkgs> {
+  config = {
+    allowUnfree = true;
+  };
+}; [
+  # # Sound
+  # alsaUtils
+  # pulseaudio-ctl
 
-  inherit
-    hello
-    git
-    neovim
-    xmonad-with-packages
-    xmobar
-  ;
-}
+  # # Fonts: Also probably need to do this through arch?
+  # # fontconfig
+  # # hack-font
+  # # inconsolata
+  # # noto-fonts-emoji
+
+  # dmenu
+  # hello
+  # git
+  # inetutils
+  # neovim
+  # rxvt-unicode
+  # slock
+  # xclip
+  # xmonad-with-packages
+  # xmobar
+  # xterm
+
+  # # X: I'm giving up on running X for now
+  # # xorg.xauth
+  # # xorg.xf86inputsynaptics
+  # # xorg.xf86videointel
+  # # xorg.xinit
+  # # xorg.xmodmap
+  # # xorg.xorgserver
+  # # xorg.xrdb
+]
